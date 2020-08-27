79db9afaf99c1078aab8147e5b4456585b481d3c
Felipelv199 <0204021@up.edu.mx>
2020-08-27 11:27:01 -0600

    Angle controller for a single model added to index.js

--- js/index.js
+++ js/index.js
@@ -42,71 +42,18 @@ function main() {
     return false;
   };
 
-  const labelX = document.createElement('label');
-  labelX.innerHTML = 'X';
-  const inputAngleX = document.createElement('input');
-  inputAngleX.type = 'number';
-  inputAngleX.value = '0';
-  inputAngleX.max = '360';
-  inputAngleX.min = '-360';
-  document.querySelector('body').appendChild(labelX);
-  document.querySelector('body').appendChild(inputAngleX);
-
-  const labelY = document.createElement('label');
-  labelY.innerHTML = 'Y';
-  const inputAngleY = document.createElement('input');
-  inputAngleY.type = 'number';
-  inputAngleY.value = '0';
-  inputAngleY.max = '360';
-  inputAngleY.min = '-360';
-  document.querySelector('body').appendChild(labelY);
-  document.querySelector('body').appendChild(inputAngleY);
-
-  const labelZ = document.createElement('label');
-  labelZ.innerHTML = 'Z';
-  const inputAngleZ = document.createElement('input');
-  inputAngleZ.type = 'number';
-  inputAngleZ.value = '0';
-  inputAngleZ.max = '360';
-  inputAngleZ.min = '-360';
-  document.querySelector('body').appendChild(labelZ);
-  document.querySelector('body').appendChild(inputAngleZ);
-
-  inputAngleX.addEventListener('change', (e) => {
-    e.preventDefault();
-    angleX = e.target.value;
-    draw(gl);
-  });
-
-  inputAngleY.addEventListener('change', (e) => {
-    e.preventDefault();
-    angleY = e.target.value;
-    draw(gl);
-  });
-
-  inputAngleZ.addEventListener('change', (e) => {
-    e.preventDefault();
-    angleZ = e.target.value;
-    draw(gl);
-  });
-
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
   draw(gl);
 }
 
 function rightClick(ev, gl) {
-  console.log(g_points);
-  if (g_points[index]) {
-    if (g_points[index] === 0) {
-      index++;
-    }
-  }
-  angleX += 10.0;
+  console.log(g_points, g_colors, g_angles);
+  index++;
   draw(gl);
 }
 
-function initVertexBuffers(gl, vertices, colors) {
+function initVertexBuffers(gl, vertices, colors, n_model) {
   var n = vertices.length / 3;
   var vertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
@@ -119,11 +66,10 @@ function initVertexBuffers(gl, vertices, colors) {
   }
   gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(a_Position);
-
   var modelMatrix = new Matrix4();
-  modelMatrix.rotate(angleX, 1, 0, 0);
-  modelMatrix.rotate(angleY, 0, 1, 0);
-  modelMatrix.rotate(angleZ, 0, 0, 1);
+  modelMatrix.rotate(g_angles[n_model][0], 1, 0, 0);
+  modelMatrix.rotate(g_angles[n_model][1], 0, 1, 0);
+  modelMatrix.rotate(g_angles[n_model][2], 0, 0, 1);
   var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
   if (!u_ModelMatrix) {
     console.log('Failed to get location of u_ModelMatrix');
@@ -170,20 +116,33 @@ function initVertexBuffers(gl, vertices, colors) {
 function draw(gl) {
   gl.clear(gl.COLOR_BUFFER_BIT);
   for (var i = 0; i < g_points.length; i++) {
+    handleAngleChange(i);
     var n = initVertexBuffers(
       gl,
       new Float32Array(g_points[i]),
-      new Float32Array(g_colors[i])
+      new Float32Array(g_colors[i]),
+      i
     );
     gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
   }
 }
-var angleX = 0.0;
-var angleY = 0.0;
-var angleZ = 0.0;
-var g_points = [[0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0]];
+
+var g_points = [];
+var g_colors = [];
+var g_angles = [];
+/* var g_points = [
+  [0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0],
+  [1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0],
+];
+var g_colors = [
+  [1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1],
+  [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0],
+];
+var g_angles = [
+  [0, 0, 0],
+  [0, 0, 0],
+]; */
 var index = g_points.length;
-var g_colors = [[1, 0.5, 1, 0, 0.5, 1, 0, 0.5, 1, 0, 0.5, 1, 1, 0.5, 1]];
 function click(ev, gl, canvas) {
   if (event.buttons == 1) {
     var x = ev.clientX;
@@ -198,6 +157,11 @@ function click(ev, gl, canvas) {
       var arrayColors = [];
       g_colors.push(arrayColors);
     }
+    if (!g_angles[index]) {
+      const angle = [0, 0, 0];
+      createInputAngleFigure(gl);
+      g_angles.push(angle);
+    }
 
     g_points[index].push(x);
     g_points[index].push(y);
@@ -209,4 +173,54 @@ function click(ev, gl, canvas) {
 
     draw(gl);
   }
+}
+
+function createInputAngleFigure(gl) {
+  const div = document.createElement('div');
+  div.id = index;
+  const labelX = document.createElement('label');
+  labelX.innerHTML = 'X';
+  const inputAngleX = document.createElement('input');
+  inputAngleX.id = 'x';
+  inputAngleX.name = '0';
+  inputAngleX.type = 'number';
+  inputAngleX.value = '0';
+  inputAngleX.max = '360';
+  inputAngleX.min = '-360';
+  div.appendChild(labelX);
+  div.appendChild(inputAngleX);
+
+  const labelY = document.createElement('label');
+  labelY.innerHTML = 'Y';
+  const inputAngleY = document.createElement('input');
+  inputAngleY.id = 'y';
+  inputAngleY.name = '1';
+  inputAngleY.type = 'number';
+  inputAngleY.value = '0';
+  inputAngleY.max = '360';
+  inputAngleY.min = '-360';
+  div.appendChild(labelY);
+  div.appendChild(inputAngleY);
+
+  const labelZ = document.createElement('label');
+  labelZ.innerHTML = 'Z';
+  const inputAngleZ = document.createElement('input');
+  inputAngleZ.id = 'z';
+  inputAngleZ.name = '2';
+  inputAngleZ.type = 'number';
+  inputAngleZ.value = '0';
+  inputAngleZ.max = '360';
+  inputAngleZ.min = '-360';
+  div.appendChild(labelZ);
+  div.appendChild(inputAngleZ);
+
+  document.querySelector('body').appendChild(div);
+}
+
+function handleAngleChange(index) {
+  const div = document.getElementById(`${index}`);
+  div.addEventListener('change', (e) => {
+    e.preventDefault();
+    g_angles[index][e.target.name] = Number(e.target.value);
+  });
 }
