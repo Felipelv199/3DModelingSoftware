09eec6255ee34103262d562aeb2ca7120ba02227
Felipelv199 <0204021@up.edu.mx>
2020-08-27 07:47:18 -0600

    add the creation of surfaces with perpective view on index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
   'attribute vec4 a_Color;\n' +
   'varying vec4 u_FragColor;\n' +
-  'uniform mat4 u_TransformMatrix;\n' +
+  'uniform mat4 u_ModelMatrix;\n' +
+  'uniform mat4 u_ViewMatrix;\n' +
+  'uniform mat4 u_ProjMatrix;\n' +
   'void main() {\n' +
-  ' gl_Position = u_TransformMatrix * a_Position;\n' +
+  ' gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;\n' +
   ' u_FragColor = a_Color;\n' +
   '}\n';
 
@@ -14,13 +16,14 @@ var FSHADER_SOURCE =
   'void main(){\n' +
   ' gl_FragColor = u_FragColor;\n' +
   '}\n';
-function main() {
-  const canvas = document.createElement('canvas');
-  canvas.width = 400;
-  canvas.height = 400;
-  document.querySelector('body').appendChild(canvas);
-  var gl = canvas.getContext('webgl');
 
+const canvas = document.createElement('canvas');
+canvas.width = 400;
+canvas.height = 400;
+document.querySelector('body').appendChild(canvas);
+const gl = canvas.getContext('webgl');
+
+function main() {
   if (!gl) {
     console.log('Failed to get the WebGL context');
     return;
@@ -41,20 +44,88 @@ function main() {
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-}
-
-function rightClick(ev, gl) {
-  if (g_points[index] !== undefined) {
-    index++;
-  }
-  if (index > 15) {
-    delta += 10;
-  }
   draw(gl);
 }
 
+function rightClick(ev, gl) {
+  index++;
+  angle += 10.0;
+  draw(gl);
+}
+
+function initVertexBuffers(gl, vertices, colors) {
+  var n = vertices.length / 3;
+  var vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW);
+
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get program for a_Position');
+    return;
+  }
+  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  var modelMatrix = new Matrix4();
+  modelMatrix.rotate(angle, 0, 0, 1);
+  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  if (!u_ModelMatrix) {
+    console.log('Failed to get location of u_ModelMatrix');
+    return;
+  }
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+
+  var viewMatrix = new Matrix4();
+  viewMatrix.setLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  var u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
+  if (!u_ViewMatrix) {
+    console.log('Failed to get location of u_ViewMatrix');
+    return;
+  }
+  gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
+
+  var projMatrix = new Matrix4();
+  projMatrix.setPerspective(120.0, 0.5, 0.1, 5.0);
+  var u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
+  if (!u_ProjMatrix) {
+    console.log('Failed to get location of u_ProjMatrix');
+    return;
+  }
+  gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  var colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.DYNAMIC_DRAW);
+
+  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
+  if (a_Color < 0) {
+    console.log('Failed to get location of a_Color');
+    return;
+  }
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Color);
+  gl.enable(gl.DEPTH_TEST);
+  gl.depthFunc(gl.LESS);
+  return n;
+}
+
+function draw(gl) {
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  for (var i = 0; i < g_points.length; i++) {
+    var n = initVertexBuffers(
+      gl,
+      new Float32Array(g_points[i]),
+      new Float32Array(g_colors[i])
+    );
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, n);
+  }
+}
+
 var index = 0;
-var delta = 0;
+var angle = 0.0;
 var g_points = [];
 var g_colors = [];
 function click(ev, gl, canvas) {
@@ -65,6 +136,7 @@ function click(ev, gl, canvas) {
 
     x = (x - rect.left - canvas.width / 2) / (canvas.width / 2);
     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+
     if (g_points.length <= index) {
       var arrayPoints = [];
       g_points.push(arrayPoints);
@@ -88,81 +160,4 @@ function click(ev, gl, canvas) {
 
     draw(gl);
   }
-}
-
-function draw(gl) {
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  for (var i = 0; i < g_points.length; i++) {
-    var n = initVertexBuffers(
-      gl,
-      new Float32Array(g_points[i]),
-      new Float32Array(g_colors[i])
-    );
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
-  }
-}
-
-function initVertexBuffers(gl, vertices, colors) {
-  var n = vertices.length / 3;
-  var vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.DYNAMIC_DRAW);
-
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get program for a_Position');
-    return;
-  }
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Position);
-
-  var radian = (delta * Math.PI) / 180.0;
-  var cosB = Math.cos(radian);
-  var sinB = Math.sin(radian);
-
-  var transformMatrix = new Float32Array([
-    cosB,
-    -sinB,
-    0,
-    0,
-    sinB,
-    cosB,
-    0,
-    0,
-    0,
-    0,
-    1,
-    0,
-    0,
-    0,
-    0,
-    1,
-  ]);
-  var u_TransformMatrix = gl.getUniformLocation(
-    gl.program,
-    'u_TransformMatrix'
-  );
-  if (!u_TransformMatrix) {
-    console.log('Failed to get location for u_TransformMatrix');
-    return;
-  }
-  gl.uniformMatrix4fv(u_TransformMatrix, false, transformMatrix);
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-  var colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, colors, gl.DYNAMIC_DRAW);
-
-  var a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  if (a_Color < 0) {
-    console.log('Failed to get location of a_Color');
-    return;
-  }
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_Color);
-  gl.enable(gl.DEPTH_TEST);
-  gl.depthFunc(gl.LESS);
-  return n;
 }
